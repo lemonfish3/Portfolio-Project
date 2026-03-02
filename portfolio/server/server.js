0cb9e1d78b835a72d6c72b0dc2ead4ecf083cd10
lemonfish3 <muqingyang2018@gmail.com>
2026-03-02 12:29:32 -0800

    image request with pexels

--- portfolio/server/server.js
+++ portfolio/server/server.js
@@ -1,15 +1,55 @@
-// Server that handles images and captions API requests 
+// imports
+require("dotenv").config()
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const axios = require("axios");
 
 app.use(cors());
 app.use(express.json());
 
+//routes
+const queries = [
+  "dog",
+  "sunset",
+  "mountain",
+  "coffee",
+  "ocean"
+]
+
+// random picker
+function getRandomQuery() {
+  return queries[Math.floor(Math.random() * queries.length)]
+}
+
+// get image from pexels
+async function getImage(query) {
+    const response = await axios.get(
+        `https://api.pexels.com/v1/search?query=${query}&per_page=1`,
+        {
+            headers: {
+                Authorization: process.env.PEXELS_API_KEY
+            }
+        }
+    )
+
+    return response.data.photos[0].src.large
+}
 
 // Sample endpoint to test the server
-app.get('/', (req, res) => {
-    res.send('Hello from the server!');
+app.get('/api/image/random', (req, res) => {
+    try {
+        const query = getRandomQuery()
+        const imageUrl = getImage(query)
+
+        res.json({
+            imageUrl,
+            caption: `Random query selected: ${query}`
+    })
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong"})
+    }
 })
 
 const Port = 5001;
