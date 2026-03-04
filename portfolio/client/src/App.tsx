fd2572c53a042c5e882d48fa6998df76d0bc1525
lemonfish3 <muqingyang2018@gmail.com>
2026-03-03 16:11:56 -0800

    fix build error

--- portfolio/client/src/App.tsx
+++ portfolio/client/src/App.tsx
@@ -4,7 +4,7 @@ import Home from './pages/home';
 import About from './pages/about';
 import Projects from './pages/projects';
 import ImageAPI from './pages/image_api';
-import Contact from './pages/contact';
+// import Contact from './pages/contact';
 
 function App() {
   return (
